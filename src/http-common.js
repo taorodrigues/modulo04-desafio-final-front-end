1a2c5f265dffe97ab7685c3a46e1170bc083e0f3
Thiago Rodrigues <taorodrigues9@gmail.com>
2020-11-09 15:56:37 -0300

    Alterando porta para chamar a API.

--- src/http-common.js
+++ src/http-common.js
@@ -2,7 +2,7 @@ import axios from 'axios';
 
 //Define a URL base da origem para consumo do servico
 export default axios.create({
-  baseURL: 'http://localhost:8081/',
+  baseURL: 'http://localhost:8080/',
   headers: {
     'Content-type': 'application/json',
   },
